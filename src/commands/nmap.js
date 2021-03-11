efd451b91c88a1971cb65c3265dcf7e51d374fba
Dan Borges <ahhh.db@gmail.com>
2021-03-11 11:10:36 -0800

    Create nmap.js

---
+++ src/commands/nmap.js
@@ -0,0 +1,41 @@
+const fetch = require("node-fetch");
+const util = require("../util");
+var spawn = require('child_process').spawn
+
+const getScan = (query) => {
+  return new Promise((resolve, reject) => {
+    run_cmd('nmap', ['--open', '-F', '-T5', '-Pn', '-sT', query], function(text) { resolve(text); });
+  })
+};
+
+function run_cmd(cmd, args, callBack ) {
+    var child = spawn(cmd, args);
+    var resp = "";
+    child.stdout.on('data', function (buffer) { resp += buffer.toString() });
+    child.stdout.on('end', function() { callBack (resp) });
+}
+
+module.exports = {
+    name: "nmap",
+    aliases: ["nscan"],
+    exec: async (msg, args) => {
+        const query = args.join(" ");
+        if (!query) return msg.channel.send(util.embed().setDescription("❌ | Missing args."));
+
+        try {
+            const res = await getScan(query);
+            const splittedRes = util.chunk(res, 1024);
+
+            const embed = util.embed()
+                .setAuthor(query)
+                .setDescription(splittedRes[0])
+                .setFooter(`Page 1 of ${splittedRes.length}.`);
+
+            const nMsg = await msg.channel.send(embed);
+            if (splittedRes.length > 1) await util.pagination(nMsg, msg.author, splittedRes);
+        } catch (e) {
+            if (e.message === "Sorry I couldn't find that results") msg.channel.send(util.embed().setDescription(`❌ | ${e.message}`));
+            else msg.channel.send(`An error occured: ${e.message}.`);   
+        }
+    }
+};
