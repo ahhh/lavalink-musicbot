87ddbecf256a2bfd090a69306edc7b7e7f7cfc3b
Dan Borges <ahhh.db@gmail.com>
2021-03-26 17:32:24 -0700

    Update warzone.js

--- src/commands/warzone.js
+++ src/commands/warzone.js
@@ -19,28 +19,32 @@ module.exports = {
 
         try {
             const res = await getDeets(platform, user);
-			console.log(res.data.matches[0].segments[0].stats);
-			var results = "Last Match \nKills: "+ res.data.matches[0].segments[0].stats.kills.value + "\n";
-			if (res.data.matches[0].segments[0].stats.assists) results = results + "Assists: " + res.data.matches[0].segments[0].stats.assists.value + "\n";
-			if (res.data.matches[0].segments[0].stats.headshots) results = results + "Headshots: " + res.data.matches[0].segments[0].stats.headshots.value + "\n";
-			if (res.data.matches[0].segments[0].stats.objectiveTeamWiped) results = results + "Team Wipes: " + res.data.matches[0].segments[0].stats.objectiveTeamWiped.value + "\n";
-			results += "Damage Delt: " + res.data.matches[0].segments[0].stats.damageDone.value + "\n";
-			results += "Damage Taken: " + res.data.matches[0].segments[0].stats.damageTaken.value + "\n";
-			results = results + "Deaths: " + res.data.matches[0].segments[0].stats.deaths.value + "\n";
-			results += "KDR: " + res.data.matches[0].segments[0].stats.kdRatio.displayValue + "\n";
-			if (res.data.matches[0].segments[0].stats.objectiveReviver) results += "Revives: " + res.data.matches[0].segments[0].stats.objectiveReviver.displayValue + "\n";
-			if (res.data.matches[0].segments[0].stats.objectiveBrKioskBuy) results += "Store Buys: " + res.data.matches[0].segments[0].stats.objectiveBrKioskBuy.displayValue + "\n";
-			results += "Placement: " +res.data.matches[0].segments[0].stats.teamPlacement.value + "\n";
-			results += "Time Played: " +res.data.matches[0].segments[0].stats.timePlayed.value + "\n";
-			results += "Time Moving: " +res.data.matches[0].segments[0].stats.percentTimeMoving.displayValue + "\n";
-
+			const matches = res.data.matches
+			let results = new Array(matches.length-1);
+			for (i = 0; i < results.length-2; i++) {
+				results[i] = "Match " + (i+1) +" \nKills: "+ res.data.matches[i].segments[0].stats.kills.value + "\n";
+				if (res.data.matches[i].segments[0].stats.assists) results[i] = results[i] + "Assists: " + res.data.matches[i].segments[0].stats.assists.value + "\n";
+				if (res.data.matches[i].segments[0].stats.headshots) results[i] = results[i] + "Headshots: " + res.data.matches[i].segments[0].stats.headshots.value + "\n";
+				if (res.data.matches[i].segments[0].stats.objectiveTeamWiped) results[i] = results[i] + "Team Wipes: " + res.data.matches[i].segments[0].stats.objectiveTeamWiped.value + "\n";
+				if (res.data.matches[i].segments[0].stats.damageDone) results[i] += "Damage Delt: " + res.data.matches[i].segments[0].stats.damageDone.value + "\n";
+				if (res.data.matches[i].segments[0].stats.damageTaken) results[i] += "Damage Taken: " + res.data.matches[i].segments[0].stats.damageTaken.value + "\n";
+				if (res.data.matches[i].segments[0].stats.deaths) results[i] = results[i] + "Deaths: " + res.data.matches[i].segments[0].stats.deaths.value + "\n";
+				if (res.data.matches[i].segments[0].stats.kdRatio) results[i] += "KDR: " + res.data.matches[i].segments[0].stats.kdRatio.displayValue + "\n";
+				if (res.data.matches[i].segments[0].stats.objectiveReviver) results[i] += "Revives: " + res.data.matches[i].segments[0].stats.objectiveReviver.displayValue + "\n";
+				if (res.data.matches[i].segments[0].stats.objectiveBrKioskBuy) results[i] += "Store Buys: " + res.data.matches[i].segments[0].stats.objectiveBrKioskBuy.displayValue + "\n";
+				if (res.data.matches[i].segments[0].stats.teamPlacement) results[i] += "Placement: " +res.data.matches[i].segments[0].stats.teamPlacement.value + "\n";
+				if (res.data.matches[i].segments[0].stats.timePlayed) results[i] += "Time Played: " +res.data.matches[i].segments[0].stats.timePlayed.value + "\n";
+				if (res.data.matches[i].segments[0].stats.percentTimeMoving) results[i] += "Time Moving: " +res.data.matches[i].segments[0].stats.percentTimeMoving.displayValue + "\n";
+			}
 
             const embed = util.embed()
                 .setTitle(user)
-				.setURL(`https://cod.tracker.gg/warzone/profile/${encodeURIComponent(platform)}/${encodeURIComponent(user)}/overview`)
-                .setDescription(results);
-
-            const myMsg = await msg.channel.send(embed);
+		.setURL(`https://cod.tracker.gg/warzone/profile/${encodeURIComponent(platform)}/${encodeURIComponent(user)}/overview`)
+                .setDescription(results[0])
+		.setFooter(`Page 1 of ${results.length}.`);
+				
+            const myMsg = await msg.channel.send(embed);				
+            if (results.length > 1) await util.pagination(myMsg, msg.author, results);
         } catch (e) {
             if (e.message === "Sorry I couldn't find that hash") msg.channel.send(util.embed().setDescription(`❌ | ${e.message}`));
             else msg.channel.send(`An error occured: ${e.message}.`);   
