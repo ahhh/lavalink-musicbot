8300f3b312e0511f8a65d22189d9b9b51e7596b5
Dan Borges <ahhh.db@gmail.com>
2021-04-03 17:19:03 -0700

    Create sneep.js

---
+++ src/commands/sneep.js
@@ -0,0 +1,24 @@
+const fetch = require("node-fetch");
+const util = require("../util");
+
+var popArray = [
+    'https://i.imgur.com/ZwdXVRv.gif',
+    'https://i.imgur.com/Z56FHaK.gif',
+	'https://media1.giphy.com/media/7CenO2Ot5xvP2/giphy.gif',
+	'https://i.pinimg.com/originals/94/4b/8d/944b8dd151a76fe018026522e494c27a.gif'
+];
+
+module.exports = {
+    name: "sneep",
+    aliases: ["sneepsneep","poppop"],
+    exec: async (msg, args) => {
+        try {
+			var randomNumber = Math.floor(Math.random()*popArray.length);
+            const embed = popArray[randomNumber];
+			await msg.channel.send(embed);
+				
+        } catch (e) {
+            msg.channel.send(`An error occured: ${e.message}.`);   
+        }
+    }
+};
