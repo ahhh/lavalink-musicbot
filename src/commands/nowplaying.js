084d46031b07c31fc8484f7d7edf912a336b1a1e
Dan Borges <ahhh.db@gmail.com>
2021-04-06 01:34:50 -0700

    Update nowplaying.js

--- src/commands/nowplaying.js
+++ src/commands/nowplaying.js
@@ -5,7 +5,7 @@ module.exports = {
     name: "nowplaying",
     aliases: ["np", "nowplay"],
     exec: (msg) => {
-		console.log(Date() + " " + msg.member.user.id + " aka " + msg.member.user.tag + " is calling " + path.basename(__filename));
+	console.log(Date() + " " + msg.member.user.id + " aka " + msg.member.user.tag + " is calling " + path.basename(__filename));
         const { music } = msg.guild;
         if (!music.player || !music.player.playing) return msg.channel.send(util.embed().setDescription("❌ | Currently not playing anything."));
         msg.channel.send(util.embed().setDescription(`🎶 | Now playing **${music.current.info.title}**.`));
