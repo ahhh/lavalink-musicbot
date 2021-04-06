25a2137034ccf850997d69d491a1c80713449437
Dan Borges <ahhh.db@gmail.com>
2021-04-06 01:54:24 -0700

    Update xkcd.js
    adding logging

--- src/commands/xkcd.js
+++ src/commands/xkcd.js
@@ -1,14 +1,16 @@
 const fetch = require("node-fetch");
 const util = require("../util");
+var path = require("path");
 
 module.exports = {
     name: "xkcd",
     aliases: ["x","comic1"],
     exec: async (msg, args) => {
         try {
-            const num = randomNumber(1,2433);
-            const embed = "https://xkcd.com/"+num+"/"
-			await msg.channel.send(embed);
+		console.log(Date() + " " + msg.member.user.id + " aka " + msg.member.user.tag + " is calling " + path.basename(__filename) + " with " + args.join(" "));
+            	const num = randomNumber(1,2433);
+            	const embed = "https://xkcd.com/"+num+"/"
+		await msg.channel.send(embed);
 				
         } catch (e) {
             msg.channel.send(`An error occured: ${e.message}.`);   
