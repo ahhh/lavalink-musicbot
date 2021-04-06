545affbc4981e9f2206889e888ebdd20ea6a81a7
Dan Borges <ahhh.db@gmail.com>
2021-04-06 01:28:16 -0700

    Update meme.js
    adding logging

--- src/commands/meme.js
+++ src/commands/meme.js
@@ -1,5 +1,6 @@
 const redditImageFetcher = require("reddit-image-fetcher");
 const util = require("../util");
+var path = require("path");
 
 const getDeets = async () => {
     const meme = await redditImageFetcher.fetch({type: 'meme'});
@@ -13,10 +14,10 @@ module.exports = {
 
 
         try {
-          const res = await getDeets();
-			    const myimg = res[0].image
-
-			    await msg.channel.send(myimg);
+       		console.log(Date() + " " + msg.member.user.id + " aka " + msg.member.user.tag + " is calling " + path.basename(__filename) + " with " + args.join(" "));
+        	const res = await getDeets();
+	    	const myimg = res[0].image;
+		await msg.channel.send(myimg);
 
         } catch (e) {
             msg.channel.send(`An error occured: ${e.message}.`);   
