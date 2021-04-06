e7076ac18a221e2f1f60aa90d793156d77c101d3
Dan Borges <ahhh.db@gmail.com>
2021-04-06 01:08:21 -0700

    Update extra.js
    adding logging

--- src/commands/extra.js
+++ src/commands/extra.js
@@ -1,11 +1,13 @@
 const fetch = require("node-fetch");
 const util = require("../util");
+var path = require("path");
 
 module.exports = {
     name: "extra",
     aliases: ["wild","comic3", "extrafabulous","fabulous"],
     exec: async (msg, args) => {
         try {
+	    console.log(Date() + " " + msg.member.user.id + " aka " + msg.member.user.tag + " is calling " + path.basename(__filename) + " with " + args.join(" "));
             const num = randomNumber(1,999);
             const embed = "https://extrafabulouscomics.com/comic/"+num
 			await msg.channel.send(embed);
