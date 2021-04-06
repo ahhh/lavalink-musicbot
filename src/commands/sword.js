dd58ead8c23f2b91b4df2d9b9a103b74b6274ab3
Dan Borges <ahhh.db@gmail.com>
2021-04-06 01:50:44 -0700

    Update sword.js
    adding logging

--- src/commands/sword.js
+++ src/commands/sword.js
@@ -1,13 +1,15 @@
 const fetch = require("node-fetch");
 const util = require("../util");
+var path = require("path");
 
 module.exports = {
     name: "sword",
     aliases: ["sw","comic4"],
     exec: async (msg, args) => {
         try {
-            const embed = "https://swordscomic.com/comic/random/"
-			await msg.channel.send(embed);
+		console.log(Date() + " " + msg.member.user.id + " aka " + msg.member.user.tag + " is calling " + path.basename(__filename) + " with " + args.join(" "));
+            	const embed = "https://swordscomic.com/comic/random/"
+		await msg.channel.send(embed);
 				
         } catch (e) {
             msg.channel.send(`An error occured: ${e.message}.`);   
