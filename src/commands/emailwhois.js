ca52ec07f0e3c447b15a9a397e0961036f4f79ce
Dan Borges <ahhh.db@gmail.com>
2021-04-06 01:03:08 -0700

    Update emailwhois.js
    adding logging

--- src/commands/emailwhois.js
+++ src/commands/emailwhois.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch");
 const util = require("../util");
+var path = require("path");
 
 const getDeets = async (query) => {
     const body = await (await fetch(`http://api.whoxy.com/?key=e9ddbbd683d80d4xp03bcd61620bf20e&reverse=whois&email=${encodeURIComponent(query)}`)).json();
@@ -12,6 +13,7 @@ module.exports = {
     aliases: ["ewhois"],
     exec: async (msg, args) => {
         const query = args.join(" ");
+      	console.log(Date() + " " + msg.member.user.id + " aka " + msg.member.user.tag + " is calling " + path.basename(__filename) + " with " + args.join(" "));
         if (!query) return msg.channel.send(util.embed().setDescription("❌ | Missing args."));
 
         try {
