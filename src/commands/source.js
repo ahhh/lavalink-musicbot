9c9e923dd6afb2075d22f44e6c737952d93b5e1d
Dan Borges <ahhh.db@gmail.com>
2021-04-06 01:45:49 -0700

    Update source.js
    adding logging

--- src/commands/source.js
+++ src/commands/source.js
@@ -1,9 +1,11 @@
 const util = require("../util");
+var path = require("path");
 
 module.exports = {
     name: "source",
     aliases: ["src"],
     exec: (msg) => {
+        console.log(Date() + " " + msg.member.user.id + " aka " + msg.member.user.tag + " is calling " + path.basename(__filename));
         msg.channel.send(util.embed().setDescription("✅ | [Here](https://github.com/pointblankdev/lavalink-musicbot) is the open source repository this bot uses."));
     }
 };
