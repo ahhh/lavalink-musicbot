f75a9764e267fba0a09cdad30ccf9b23fbf03d62
Dan Borges <ahhh.db@gmail.com>
2021-04-06 01:37:39 -0700

    Update ping.js
    adding logging

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,6 +1,8 @@
+var path = require("path");
 module.exports = {
     name: "ping",
     exec: (msg) => {
+        console.log(Date() + " " + msg.member.user.id + " aka " + msg.member.user.tag + " is calling " + path.basename(__filename));
         msg.channel.send(`🏓 Pong! \`${msg.client.ws.ping}ms\``);
     }
 };
