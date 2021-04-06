94f175f29148506f2dbebc27fdd53008a401e574
Dan Borges <ahhh.db@gmail.com>
2021-04-06 01:34:39 -0700

    Update nowplaying.js
    adding logging

--- src/commands/nowplaying.js
+++ src/commands/nowplaying.js
@@ -1,9 +1,11 @@
 const util = require("../util");
+var path = require("path");
 
 module.exports = {
     name: "nowplaying",
     aliases: ["np", "nowplay"],
     exec: (msg) => {
+		console.log(Date() + " " + msg.member.user.id + " aka " + msg.member.user.tag + " is calling " + path.basename(__filename));
         const { music } = msg.guild;
         if (!music.player || !music.player.playing) return msg.channel.send(util.embed().setDescription("❌ | Currently not playing anything."));
         msg.channel.send(util.embed().setDescription(`🎶 | Now playing **${music.current.info.title}**.`));
