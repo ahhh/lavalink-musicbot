63bbf5eacb35952808f8d74c522b005e37055bf3
Dan Borges <ahhh.db@gmail.com>
2021-04-06 01:05:00 -0700

    Update explosm.js

--- src/commands/explosm.js
+++ src/commands/explosm.js
@@ -1,11 +1,13 @@
 const fetch = require("node-fetch");
 const util = require("../util");
+var path = require("path");
 
 module.exports = {
     name: "explosm",
     aliases: ["ex","comic2"],
     exec: async (msg, args) => {
         try {
+	    console.log(Date() + " " + msg.member.user.id + " aka " + msg.member.user.tag + " is calling " + path.basename(__filename) + " with " + args.join(" "));
             const num = randomNumber(1,5812);
             const embed = "https://explosm.net/comics/"+num
 			await msg.channel.send(embed);
